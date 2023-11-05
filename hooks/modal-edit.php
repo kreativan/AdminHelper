f655e784af151eb982a90977188dbe9a0d9bfe8e
Ivan Milincic <kreativan.dev@gmail.com>
2023-11-05 21:08:15 +0200

    add remove_tabs GET variable option to page-edit-modal

--- hooks/modal-edit.php
+++ hooks/modal-edit.php
@@ -10,12 +10,11 @@ namespace ProcessWire;
  * On front-end edit modal
  * Remove delete and settings tab from FieldsetPage
  */
-if ($this->input->get->modal && $this->input->get->front_end_modal) {
+if ($this->input->get->modal && ($this->input->get->front_end_modal || $this->input->get->remove_tabs)) {
 
   $class_name = $this->pages->get($this->input->get->id)->className();
 
   if ($class_name == "FieldsetPage") {
-
     $this->addHookAfter('ProcessPageEdit::buildFormDelete', function ($event) {
       $wrapper = $event->return;
       $wrapper->collapsed = Inputfield::collapsedHidden;
