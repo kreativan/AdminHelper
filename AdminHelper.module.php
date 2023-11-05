db65f1cb1838bf1302dd8fed975fa35ad5d58ebb
Ivan Milincic <kreativan.dev@gmail.com>
2023-11-05 20:59:35 +0200

    move modal-edit related stuff to hooks folder

--- AdminHelper.module.php
+++ AdminHelper.module.php
@@ -180,28 +180,6 @@ class AdminHelper extends WireData implements Module {
         $this->config->tracyDisabled = true;
         $this->config->styles->append($this->url() . "assets/css/page-edit-modal-page-table.css{$suffix}");
       }
-
-      /**
-       * On front-end edit modal
-       * Remove delete and settings tab from FieldsetPage
-       */
-      if ($this->input->get->modal && $this->input->get->front_end_modal) {
-        $class_name = $this->pages->get($this->input->get->id)->className();
-        if ($class_name == "FieldsetPage") {
-          $this->addHookAfter('ProcessPageEdit::buildFormDelete', function ($event) {
-            $wrapper = $event->return;
-            $wrapper->collapsed = Inputfield::collapsedHidden;
-            $process = $event->object;
-            $process->removeTab('ProcessPageEditDelete');
-          });
-          $this->addHookAfter('ProcessPageEdit::buildFormSettings', function ($event) {
-            $wrapper = $event->return;
-            $wrapper->collapsed = Inputfield::collapsedHidden;
-            $process = $event->object;
-            $process->removeTab('ProcessPageEditSettings');
-          });
-        }
-      }
     }
   }
 
---
+++ hooks/modal-edit.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Modal Edit
+ */
+
+namespace ProcessWire;
+
+/**
+ * On front-end edit modal
+ * Remove delete and settings tab from FieldsetPage
+ */
+if ($this->input->get->modal && $this->input->get->front_end_modal) {
+
+  $class_name = $this->pages->get($this->input->get->id)->className();
+
+  if ($class_name == "FieldsetPage") {
+
+    $this->addHookAfter('ProcessPageEdit::buildFormDelete', function ($event) {
+      $wrapper = $event->return;
+      $wrapper->collapsed = Inputfield::collapsedHidden;
+      $process = $event->object;
+      $process->removeTab('ProcessPageEditDelete');
+    });
+
+    $this->addHookAfter('ProcessPageEdit::buildFormSettings', function ($event) {
+      $wrapper = $event->return;
+      $wrapper->collapsed = Inputfield::collapsedHidden;
+      $process = $event->object;
+      $process->removeTab('ProcessPageEditSettings');
+    });
+  }
+}
